d70de3293787e982938c41b8cdee2158826f8351
a776058959 <776058959@qq.com>
2022-11-03 13:56:54 +0800

    Update mihoyo.com获取cookie.user.js

--- mihoyo.com获取cookie.user.js
+++ mihoyo.com获取cookie.user.js
@@ -6,7 +6,7 @@
 // @author      EA乖离
 // @description 米游社获取cookie
 // @grant       GM_registerMenuCommand
-// @updateurl    https://github.com/a776058959/MihoyoBBSGetCookie/raw/main/mihoyo.com%E8%8E%B7%E5%8F%96cookie.user.js
+// @updateurl   https://github.com/a776058959/MihoyoBBSGetCookie/raw/main/mihoyo.com%E8%8E%B7%E5%8F%96cookie.user.js
 // ==/UserScript==
 (function() {
   'use strict';
